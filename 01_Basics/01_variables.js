c7186d60d102cf8e9c38118321de88aba66de6de
Anand Krishna Mishra <160732081+amishr87@users.noreply.github.com>
2025-01-26 11:41:18 +0000

    lec 5

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,5 @@
+const id = 53423
+
+let name = "Anand"
+
+console.table([id, name])
---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,10 @@
+let id = 453
+let name = "Anand"
+let isLoggedIn = true
+let state = null
+
+
+console.table([typeof id, typeof name, typeof isLoggedIn, typeof state])
+
+console.log(typeof undefined)       //undefined
+console.log(typeof null)            //object
