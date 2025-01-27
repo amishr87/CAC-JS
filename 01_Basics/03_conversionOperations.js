74fd0a08b44cf63cf9eb92c0816b42d3230b537a
Anand Krishna Mishra <160732081+amishr87@users.noreply.github.com>
2025-01-27 07:00:16 +0000

    lec 6

---
+++ 01_Basics/03_conversionOperations.js
@@ -0,0 +1,27 @@
+let score = "33"
+let scoreWrong = "33abc"
+
+console.log(typeof score);
+console.log(typeof scoreWrong);
+
+let valueInNumber = Number(score)
+let valueInNumber2 = Number(scoreWrong)
+
+console.log(typeof valueInNumber, valueInNumber);
+console.log(typeof valueInNumber2, valueInNumber2);  //NaN means Non a Number
+
+//33 => 33
+//"33abc" => NaN
+//true => 1; false => 0
+//null => 0
+//undefined => NaN
+
+let isLoggedIn = 1
+
+let boolIsLoggedIn = Boolean(isLoggedIn);
+console.log(boolIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "abc" => true
+// "false" => true, as "false" is treated as a non-empty string
