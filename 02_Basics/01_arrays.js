42c2e7148130e05a81c459014a78f82692413974
Anand Krishna Mishra <160732081+amishr87@users.noreply.github.com>
2025-01-28 23:39:35 +0000

    lect 14 prog

--- 02_Basics/01_arrays.js
+++ 02_Basics/01_arrays.js
@@ -1,5 +1,16 @@
-const Arr = [1, 2, 3, 4, 5]
+const arr = [1, 2, 3, 4, 5]
 const myHeroes = ["Batman", "Flash"]
 
-console.log(Arr[0]);
+console.log(arr[0]);
 console.log(myHeroes[1]);
+
+const arr2 = new Array(1, 2, 3, 4)
+console.log(arr2[1]);
+
+//Array Methods
+arr2.push(6)
+console.log(arr2);
+
+arr2.pop()
+
+console.log(arr2);
