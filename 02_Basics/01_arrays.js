b942cc255592ee241f8e11fcec0bb0b934c88878
Anand Krishna Mishra <160732081+amishr87@users.noreply.github.com>
2025-01-29 00:34:33 +0000

    lec 14

--- 02_Basics/01_arrays.js
+++ 02_Basics/01_arrays.js
@@ -8,9 +8,40 @@ const arr2 = new Array(1, 2, 3, 4)
 console.log(arr2[1]);
 
 //Array Methods
-arr2.push(6)
+
+//push adds to end
+arr2.push(6);
 console.log(arr2);
 
-arr2.pop()
-
+//pop removes from end
+arr2.pop();
 console.log(arr2);
+
+//unshift adds to the start
+arr2.unshift(9);
+console.log(arr2);
+
+//shift removes from start
+arr2.shift();
+console.log(arr2);
+
+console.log(arr2.includes(4));
+console.log(arr2.indexOf(3));
+console.log(arr2.indexOf(18));
+
+//join converts array to a string with its elements separated by a comma
+const newArr = arr2.join();
+console.log(newArr);
+console.log(typeof newArr);
+
+//slice does not change original array
+//it accesses elements from index 1 to 3-1
+const newArr2 = arr2.slice(1,3);
+console.log(newArr2); //slice array
+console.log(arr2); //original array
+
+//splice changes the original array by subtracting the spliced part
+//it accesses elements from index 1 to 3
+const newArr3 = arr2.splice(1,3);
+console.log(newArr3); //slice array
+console.log(arr2); //original array
