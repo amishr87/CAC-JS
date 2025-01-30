3d65d974db64c1b36d3b4eacc22111e71c8ff609
Anand Krishna Mishra <160732081+amishr87@users.noreply.github.com>
2025-01-30 02:42:42 +0000

    lec 20

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -47,3 +47,48 @@ function testUserMessage(username = "Someone"){
 
 console.log(testUserMessage());
 console.log();
+
+//In case number of parameters are not known
+// ... is known as the spread operator or the rest operator depending on its use
+// Here it is called rest operator
+function calculateCartPrice(...num1){
+    return num1
+}
+
+console.log(calculateCartPrice(200, 300, 500));
+console.log();
+
+//another format
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+console.log(calculateCartPrice(200, 300, 500, 800, 900)); //200 is stored in val1, 300 in val2, and the rest in num1
+console.log();
+
+const user = {
+    user: "Anand",
+    price: 199,
+}
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.user} and price is ${anyObject.price}`);
+}
+
+handleObject(user)
+console.log();
+
+//make an object while calling the funcion
+handleObject({
+    user: "Sam",
+    price: 499
+})
+console.log();
+
+const myNewArray = [1, 2, 3, 4, 5]
+
+function returnArray(array){
+    return array[1]
+}
+
+console.log(returnArray(myNewArray));
