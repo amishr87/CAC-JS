ae7a1cd40f61b65e19ed240f9c53529e935e6dde
Anand Krishna Mishra <160732081+amishr87@users.noreply.github.com>
2025-03-14 23:58:01 +0000

    lec 40

--- 09_Advance/02_promises.js
+++ 09_Advance/02_promises.js
@@ -1,6 +1,6 @@
 const promiseOne = new Promise(function(resolve, reject){
-    //Do an async task
-    // DB calls, cryptography, network
+    //  Do an async task
+    //  DB calls, cryptography, network
     setTimeout(function(){
         console.log('Async task is compelete');
         resolve()
@@ -11,6 +11,7 @@ promiseOne.then(function(){
     console.log("Promise consumed");
 })
 
+//Another way to do the same thing as above 
 new Promise(function(resolve, reject){
     setTimeout(function(){
         console.log("Async task 2");
@@ -20,3 +21,89 @@ new Promise(function(resolve, reject){
 }).then(function(){
     console.log("Async 2 resolved");
 })
+
+const promiseThree = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        console.log('Async task 3');
+        resolve({username: "Anand", email: "amishr87@asu.edu"})
+    }, 1000)
+})
+
+promiseThree.then(function(user){
+    console.log(user);
+    console.log("Async 3 resolved");
+});
+
+const promiseFour = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = true;
+        if(!error){
+            resolve({username: "Anand", email: "amishr87@asu.edu"})
+        }else{
+            reject("ERROR: Something went worng")
+        }
+    }, 1000)
+});
+
+promiseFour
+.then(function(user){
+    console.log(user);
+    return user.username;
+})
+.then(function(username){
+    console.log(username);
+})
+.catch(function(error){
+    console.log(error);
+})
+.finally(function(){
+    console.log("The promise is either resolved or rejected")
+})
+
+
+const promiseFive = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = true;
+        if(!error){
+            resolve({username: "javascript_user", email: "jsuser@asu.edu"})
+        }else{
+            reject("ERROR: JS went worng")
+        }
+    }, 1000)
+})
+
+async function consumePromiseFive(){
+    try {
+        const response = await promiseFive
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+consumePromiseFive()
+
+// async function getAllUsers(){
+//     try {
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+
+//         const data = await response.json()
+//         console.log(data);
+//     } catch (error) {
+//         console.log("E: ", error);
+//     }
+// }
+
+//getAllUsers()
+
+fetch('https://api.github.com/users/hiteshchoudhary')
+.then((response) => {
+    return response.json()
+})
+.then((data) => {
+    console.log(data);
+})
+.catch((error) => console.log(error))
+
+// promise.all
+// yes this is also available, kuch reading aap b kro.
