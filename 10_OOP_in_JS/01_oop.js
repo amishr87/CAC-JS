ba6213d38ba624b33057ad27d533219a073975c1
Anand Krishna Mishra <160732081+amishr87@users.noreply.github.com>
2025-03-15 23:52:35 +0000

    lec 42

---
+++ 10_OOP_in_JS/01_oop.js
@@ -0,0 +1,41 @@
+const user = {
+    username: "hitesh",
+    loginCount: 8,
+    signedIn: true,
+
+    getUserDetails: function(){
+        //console.log("Got user details from database");
+        // console.log(`Username: ${this.username}`);
+        console.log(this);
+    }
+
+}
+
+
+
+//console.log(user.username)
+//console.log(user.getUserDetails());
+// console.log(this);
+
+
+function User(username, loginCount, isLoggedIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn
+
+    this.greeting = function(){
+        console.log(`Welcome ${this.username}`);
+
+    }
+
+    //return this                                         //no need to write this as it is implicitly called (automatically called)
+}
+
+const userOne = new User("hitesh", 12, true)
+const userTwo = new User("ChaiAurCode", 11, false)
+console.log(userOne.constructor);
+//console.log(userTwo);
+
+// new keyword does the following:
+// 1)   creates a new empty object
+// 2)   then calls the constructure function which packets all the arguments you created while making the funciton and then passes them into this new instance object
